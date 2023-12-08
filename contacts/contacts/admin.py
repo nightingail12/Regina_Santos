da51d366c1bf07ce3eb6341c213a7bfd2c6acf66
nightingail12 <150108650+nightingail12@users.noreply.github.com>
2023-12-08 13:35:00 -0800

    Foundation
    Foundation of contact form

---
+++ contacts/contacts/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from contacts.models import crudst
+
+admin.site.register(crudst)
---
+++ contacts/contacts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class crudst(models.Model):
+    contact_name = models.CharField(max_length = 100)
+    contact_email = models.CharField(max_length = 100)
+    contact_notes = models.CharField(max_length = 500)
