aa0809bf82d1e3282dea73e82aa15ff233b52edb
nightingail12 <150108650+nightingail12@users.noreply.github.com>
2023-12-10 14:14:01 -0800

    Edits to show timestamp

--- contacts/contacts/models.py
+++ contacts/contacts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class crudst(models.Model):
-    contact_name = models.CharField(max_length = 100)
-    contact_email = models.CharField(max_length = 100)
-    contact_notes = models.CharField(max_length = 500)
-    contact_time = models.DateField()
+    contact_name = models.CharField(max_length=100)
+    contact_email = models.EmailField()
+    contact_notes = models.CharField(max_length=500)
+    contact_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.contact_name
 
 
--- contacts/contacts/views.py
+++ contacts/contacts/views.py
@@ -1,3 +1,5 @@
+# views.py
+
 from django.shortcuts import render
 from contacts.models import crudst
 from django.contrib import messages
@@ -6,14 +8,16 @@ def contact_display(request):
     results = crudst.objects.all()
     return render(request, 'index.html', {'crudst': results})
 
+
 def contact_insert(request):
     if request.method == "POST":
-        if request.POST.get('contact_name') and request.POST.get('contact_email'):
+        if request.POST.get('contact_name') and request.POST.get('contact_email') or request.POST.get('contact_notes'):
             contact_saved = crudst()
             contact_saved.contact_name = request.POST.get('contact_name')
             contact_saved.contact_email = request.POST.get('contact_email')
+            contact_saved.contact_notes = request.POST.get('contact_notes')
             contact_saved.save()
-            messages.success(request, '<contact_saved.contact_name> has been added.')
+            messages.success(request, f'{contact_saved.contact_name} has been added.')
             return render(request,'new_contact.html')
     else:
         return render(request,'new_contact.html')
